85b6986059bdd5fb24262dcd5763f1bcd4babdc0
Sergey Pozdnyakov <mislikr45@gmail.com>
2023-08-08 19:05:19 +0500

    1

--- app.js
+++ app.js
@@ -28,9 +28,6 @@ app.use((req, res, next) => {
 
   next();
 });
-app.use((req, res, next) => {
-  next(res.status(404).send({ message: 'Ресурс не найден' }));
-});
 app.use(routesUser);
 app.use(routerCards);
 app.listen(PORT, () => { });
