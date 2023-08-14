96525059108971eba050b33b39f1a2757dc08a8b
Sergey Pozdnyakov <mislikr45@gmail.com>
2023-08-14 23:01:03 +0500

    1

--- app.js
+++ app.js
@@ -32,6 +32,14 @@ mongoose.connect(
 //   next();
 // });
 
+app.use((req, res, next) => {
+  if (req.url === '/signup' || req.url === '/signin') {
+    next(); // Skip auth for signup and signin
+  } else {
+    auth(req, res, next); // Apply authMiddleware for other routes
+  }
+});
+
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     password: Joi.string().required().min(8),
