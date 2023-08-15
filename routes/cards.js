aa9f05a8715d0d82337f95914ea4ef8f0310bf14
Sergey Pozdnyakov <mislikr45@gmail.com>
2023-08-15 15:21:54 +0500

    1

--- routes/cards.js
+++ routes/cards.js
@@ -1,12 +1,16 @@
 const router = require('express').Router();
 const auth = require('../middlewares/auth');
+const { celebrate, Joi } = require('celebrate');
 
 const {
   getCards, createCard, deleteCard, addLikeCard, deleteLikeCard,
 } = require('../controllers/cards');
 
 // роутер /cards
-router.get('/cards', getCards);
+router.get('/cards', celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string().length(24).hex().required(),
+  })}), getCards);
 
 router.post('/cards', createCard);
 
