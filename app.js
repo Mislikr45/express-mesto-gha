a76c9edb014a0af07a6e2f668e11585b1120c1b7
Sergey Pozdnyakov <mislikr45@gmail.com>
2023-08-17 11:39:18 +0500

    1

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const cookies = require('cookie-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
@@ -17,7 +16,7 @@ const app = express();
 app.use(express.json());
 app.use(cookies());
 // app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 mongoose.connect(
