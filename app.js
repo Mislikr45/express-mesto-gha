844d2ec888b51ea0878e5441345842c1b1395eca
Sergey Pozdnyakov <mislikr45@gmail.com>
2023-08-17 22:16:43 +0500

    2

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const routesUser = require('./routes/users');
 const routerCards = require('./routes/cards');
 const { login, createUser } = require('./controllers/users');
 const errorHandler = require('./middlewares/errorHandler');
+
+const noWay = require('./middlewares/noWay');
 const { URL_REGEX } = require('./utils/constants');
 
 const { PORT = 3000 } = process.env;
@@ -43,5 +45,6 @@ app.post('/signup', celebrate({
 app.use(routesUser);
 app.use(routerCards);
 app.use(errors());
+app.use(noWay);
 app.use(errorHandler);
 app.listen(PORT, () => { });
