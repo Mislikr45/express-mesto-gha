6454480426049e07da2b836d85eb6244a1380bb4
Sergey Pozdnyakov <mislikr45@gmail.com>
2023-08-17 12:24:59 +0500

    1

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -10,16 +10,16 @@ const auth = (req, res, next) => {
   const authorization = req.cookies.jwt;
   if (!authorization || !authorization.startsWith('')) {
     return handleAuthError(res.status(401).send({ message: 'Необходима авторизация' }));
-  } else {
+  }
   const token = authorization.replace('Bearer ', '');
   let payload;
   try {
     payload = jwt.verify(token, 'super-strong-secret');
   } catch (err) {
     return handleAuthError(res);
-  }} else {
+  }
   req.user = payload._id; // записываем пейлоуд в объект запроса
-  next();}
+  return next();
 };
 
 module.exports = auth;
