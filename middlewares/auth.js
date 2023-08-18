17714b348a9c5cddd5669a0db5d64b1ecfea098d
Sergey Pozdnyakov <mislikr45@gmail.com>
2023-08-18 12:41:40 +0500

    1

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,11 +1,11 @@
 const jwt = require('jsonwebtoken');
+const AuthorizationError = require('../errors/AuthorizationError');
 
-
-const handleAuthError = (res) => {
-  res
-    .status(401)
-    .send({ message: 'Необходима авторизация' });
-};
+// const handleAuthError = (res) => {
+//   res
+//     .status(401)
+//     .send({ message: 'Необходима авторизация' });
+// };
 
 // const handleAuthError = (err, res, next) => {
 //   const statusCode = err.statusCode || 401;
@@ -18,14 +18,14 @@ const handleAuthError = (res) => {
 const auth = (req, res, next) => {
   const authorization = req.cookies.jwt;
   if (!authorization || !authorization.startsWith('')) {
-    return handleAuthError(res);
+    return next(new AuthorizationError('Необходима авторизация'));
   }
   const token = authorization.replace('Bearer ', '');
   let payload;
   try {
     payload = jwt.verify(token, 'super-strong-secret');
   } catch (err) {
-    return handleAuthError(res);
+    return next(new AuthorizationError('Необходима авторизация'));
   }
   req.user = payload._id; // записываем пейлоуд в объект запроса
   return next();
